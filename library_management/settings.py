ecf384e8d912416d6bbe83192f53506ac212105d
Okyesco <okyesco0549677744@gmail.com>
2023-06-14 10:19:49 +0000

    new commit

--- library_management/settings.py
+++ library_management/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'library',
-    'authentication'
+    'authentication',
+    'django_admin_logs',
 ]
 
 MIDDLEWARE = [
@@ -134,3 +135,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'pics')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+DJANGO_ADMIN_LOGS_DELETABLE = True
